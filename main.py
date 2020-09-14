1223770cd8e53ad44d89b0afd9b1b2594866b5dd
Abirami <65455645+abby711@users.noreply.github.com>
2020-09-14 21:36:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:abby@localhost/quotes'
-app.config['SQLALCHEMY_DATABASE_URI'] ='postgres://rqaxnukzqkwqax:bede3b5c46b7fc2f134b386b56a99d2946ea3e1b2653a7c2827113327f8918fb@ec2-52-48-65-240.eu-west-1.compute.amazonaws.com:5432/d8fhdtg045n27t'
+# using pgadmin psql db
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:yourpassword@localhost/quotes'
+#to deploy using heroku
+#app.config['SQLALCHEMY_DATABASE_URI'] ='postgres://rqaxnukzqkwqax:bede3b5c46b7fc2f134b386b56a99d2946ea3e1b2653a7c2827113327f8918fb@ec2-52-48-65-240.eu-west-1.compute.amazonaws.com:5432/d8fhdtg045n27t'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
 dbb = SQLAlchemy(app)
 
